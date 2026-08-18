dc586821c0dea04966776ea578b64ceb88647f56
DeWebAlpha <tribillio@gmail.com>
2026-08-18 13:06:49 +0000

    add constants barrel file

---
+++ backend/src/constants/index.js
@@ -0,0 +1,7 @@
+/** Barrel re-exporting every constant in this folder. */
+export { HTTP_STATUS } from "./httpStatus.js";
+export { OPTIONS } from "./mongoose.options.js";
+export { SENSITIVE_FIELDS } from "./sensitiveFields.js";
+export { ARGON_CONFIG } from "./argonConfig.js";
+export { SECURITY_CONFIG } from "./security.js";
+export { SUBJECTS } from "./subjects.js";
